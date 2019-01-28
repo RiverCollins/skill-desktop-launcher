83a136b837061660e04be290a64de0d6021a7eba
River Collins <riverc@deltatech.tech>
2019-01-27 20:47:12 -0600

    getting it to work

--- __init__.py
+++ __init__.py
@@ -23,16 +23,16 @@ from os.path import dirname, join
 
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
-if sys.version_info[0] < 3:
-    from urllib import quote
-    try:
-        import gio
-    except ModuleNotFoundError:
-        sys.path.append("/usr/lib/python2.7/dist-packages")
-        import gio
-else:
-    from urllib.parse import quote
-    from gi.repository import Gio as gio
+# if sys.version_info[0] < 3:
+#     from urllib import quote
+#     try:
+#         import gio
+#     except ModuleNotFoundError:
+#         sys.path.append("/usr/lib/python2.7/dist-packages")
+#         import gio
+# else:
+#     from urllib.parse import quote
+#     from gi.repository import Gio as gio
 
 logger = getLogger(__name__)
 __author__ = 'seanfitz'
@@ -48,33 +48,33 @@ class DesktopLauncherSkill(MycroftSkill):
     def initialize(self):
         tokenizer = EnglishTokenizer()
 
-        for app in gio.app_info_get_all():
-            name = app.get_name().lower()
-            entry = [app]
-            tokenized_name = tokenizer.tokenize(name)[0]
+        # for app in gio.app_info_get_all():
+        #     name = app.get_name().lower()
+        #     entry = [app]
+        #     tokenized_name = tokenizer.tokenize(name)[0]
 
-            if name in self.appmap:
-                self.appmap[name] += entry
-            else:
-                self.appmap[name] = entry
+        #     if name in self.appmap:
+        #         self.appmap[name] += entry
+        #     else:
+        #         self.appmap[name] = entry
 
-            self.register_vocabulary(name, "Application")
-            if name != tokenized_name:
-                self.register_vocabulary(tokenized_name, "Application")
-                if tokenized_name in self.appmap:
-                    self.appmap[tokenized_name] += entry
-                else:
-                    self.appmap[tokenized_name] = entry
+        #     self.register_vocabulary(name, "Application")
+        #     if name != tokenized_name:
+        #         self.register_vocabulary(tokenized_name, "Application")
+        #         if tokenized_name in self.appmap:
+        #             self.appmap[tokenized_name] += entry
+        #         else:
+        #             self.appmap[tokenized_name] = entry
 
-        launch_intent = IntentBuilder(
-            "LaunchDesktopApplicationIntent").require("LaunchKeyword").require(
-            "Application").build()
-        self.register_intent(launch_intent, self.handle_launch_desktop_app)
+        # launch_intent = IntentBuilder(
+        #     "LaunchDesktopApplicationIntent").require("LaunchKeyword").require(
+        #     "Application").build()
+        # self.register_intent(launch_intent, self.handle_launch_desktop_app)
 
-        close_intent = IntentBuilder(
-            "CloseDesktopApplicationIntent").require("CloseKeyword").require(
-            "Application").build()
-        self.register_intent(close_intent, self.handle_close_desktop_app)
+        # close_intent = IntentBuilder(
+        #     "CloseDesktopApplicationIntent").require("CloseKeyword").require(
+        #     "Application").build()
+        # self.register_intent(close_intent, self.handle_close_desktop_app)
 
         launch_website_intent = IntentBuilder(
             "LaunchWebsiteIntent").require("LaunchKeyword").require(
@@ -86,15 +86,15 @@ class DesktopLauncherSkill(MycroftSkill):
             "SearchTerms").build()
         self.register_intent(search_website, self.handle_search_website)
 
-    def handle_launch_desktop_app(self, message):
-        app_name = message.data.get('Application')
-        apps = self.appmap.get(app_name)
-        if apps and len(apps) > 0:
-            apps[0].launch()
+    # def handle_launch_desktop_app(self, message):
+    #     app_name = message.data.get('Application')
+    #     apps = self.appmap.get(app_name)
+    #     if apps and len(apps) > 0:
+    #         apps[0].launch()
 
-    def handle_close_desktop_app(self, message):
-        app_name = message.data.get('Application')
-        subprocess.call( [ "killall", "-9", app_name ] )
+    # def handle_close_desktop_app(self, message):
+    #     app_name = message.data.get('Application')
+    #     subprocess.call( [ "killall", "-9", app_name ] )
 
     def handle_launch_website(self, message):
         site = message.data.get("Website")
